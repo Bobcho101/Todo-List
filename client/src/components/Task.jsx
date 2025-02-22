ab20427a85d744cd366ed7388f11cfd0c8529580
Bobcho101 <bgbobi878@gmail.com>
2025-02-22 13:52:42 +0200

    made the change status handler with db call

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -3,8 +3,26 @@ import { useEffect, useState } from "react"
 export default function Task({task}){
     const [status, setStatus] = useState(task.isCompleted);
     
-    const changeStatusHandler = () => {
-        setStatus(prevStatus => !prevStatus);  
+
+    const changeStatusHandler = async () => {
+        const updatedStatus = !status;
+        try {
+            const response = await fetch(`http://localhost:3030/jsonstore/todos/${task._id}`, {
+                method: "PUT", 
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    id: task._id,
+                    text: task.text,
+                    isCompleted: updatedStatus
+                }),
+            });
+
+            setStatus(updatedStatus); 
+        } catch (err) {
+            console.log(err.message);
+        }
     };
 
     return(
