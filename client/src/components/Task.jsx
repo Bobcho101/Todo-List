34787431b014b69b290327e50ca62e6826f51cbd
Bobcho101 <bgbobi878@gmail.com>
2025-02-22 13:41:28 +0200

    added a change status without db requests for now

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -1,11 +1,19 @@
+import { useEffect, useState } from "react"
+
 export default function Task({task}){
+    const [status, setStatus] = useState(task.isCompleted);
+    
+    const changeStatusHandler = () => {
+        setStatus(prevStatus => !prevStatus);  
+    };
+
     return(
-    <tr className={task.isCompleted ? 'todo is-completed' : 'todo'}>
-        <td>{task.text}</td> 
-        <td>{task.isCompleted ? 'Completed' : 'Not Completed'}</td>
-        <td className="todo-action">
-        <button className="btn todo-btn">Change status</button>
-        </td>
-    </tr>
+        <tr className={status ? 'todo is-completed' : 'todo'}>
+            <td>{task.text}</td> 
+            <td>{status ? 'Completed' : 'Not Completed'}</td>
+            <td className="todo-action">
+            <button onClick={() => changeStatusHandler()} className="btn todo-btn">Change status</button>
+            </td>
+        </tr>
     )
 }
