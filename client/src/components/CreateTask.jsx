85126a54187bd432b1b5b3b906542313a714d64d
Bobcho101 <bgbobi878@gmail.com>
2025-02-23 12:53:41 +0200

    added the api call for the create task

--- client/src/components/CreateTask.jsx
+++ client/src/components/CreateTask.jsx
@@ -1,10 +1,34 @@
 export default function CreateTask({minimizeBtn}){
 
+    async function formHandler(e){
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const text = formData.get('text');
+        if(text.trim() === "") return;
+        
+        try{
+            const response = await fetch('http://localhost:3030/jsonstore/todos', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    text,
+                    isCompleted: false
+                })
+            })
+            minimizeBtn();
+
+        } catch(err){
+            console.log(err.message);
+        }
+    
+    }
     return (
     <>
         <div id="create-box">
             <div id="x" onClick={() => minimizeBtn()}>❌</div>
-            <form>
+            <form onSubmit={formHandler}>
                 <div className="create-form-el">
                     <label htmlFor="text">Task name: </label>
                     <input type="text" name="text" />
